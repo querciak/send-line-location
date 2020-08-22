f8879d302c69ce0fc7062e494aa53c1f53ddc230
Andrea Cappello <andrea.cappello@elisa.fi>
2020-08-22 14:54:24 +0300

    improved landing page

--- pages/index.js
+++ pages/index.js
@@ -6,56 +6,37 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>SlackTN ASD</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://www.instagram.com/slackline_trentino/" className={styles.iglink}> Slackline Trentino</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Safety is our first priority!{' '}
+          {/* <code className={styles.code}>pages/index.js</code> */}
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
 
           <a
             href="posts/info-about-me"
             className={styles.card}
           >
-            <h3>Info about me &rarr;</h3>
+            <h3>Report you line &rarr;</h3>
             <p>
-              Learn more about the developer.
+              Notify your position to the 
+              <text className={styles.bold}> helicopter rescue core</text>.
+            </p>
+          </a>
+
+          <a href="posts/info-about-me" className={styles.card}>
+            <h3>Info about us &rarr;</h3>
+            <p>
+              Learn more about our association.
             </p>
           </a>
 
